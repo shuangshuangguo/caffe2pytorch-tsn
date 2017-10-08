b20953a1abe076a1e948c24587f565181d97f431
guoshuangshuang <31126075+gss-ucas@users.noreply.github.com>
2017-10-08 10:30:57 +0800

    Update caffe2pytorch.sh

--- caffe2pytorch.sh
+++ caffe2pytorch.sh
@@ -6,12 +6,12 @@ stream=$2
 
 echo "transfer caffemodel to pytorchmodel for TSN:"
 echo "first get .hdf5 file:"
-if python export_to_hdf5.py --model ${dataset}_split_1_tsn_${stream}_reference_bn_inception.caffemodel --hdf5 ./model/${dataset}_1_${stream}.h5; then
+if python export_to_hdf5.py --model ${dataset}_split_1_tsn_${stream}_reference_bn_inception.caffemodel --hdf5 ${dataset}_1_${stream}.h5; then
     echo "then get torchmodel:"
-    th googlenet.lua ${dataset} ${stream} ./model/${dataset}_1_${stream}.h5 ./model/${dataset}_1_${stream}.t7
-    if th googlenet.lua ${dataset} ${stream} ./model/${dataset}_1_${stream}.h5 ./model/${dataset}_1_${stream}.t7; then
+    th googlenet.lua ${dataset} ${stream} ${dataset}_1_${stream}.h5 ${dataset}_1_${stream}.t7
+    if th googlenet.lua ${dataset} ${stream} ${dataset}_1_${stream}.h5 ${dataset}_1_${stream}.t7; then
         echo "finally transfer torchmodel to pytorchmodel"
-        python convert_torch.py --t7 ./model/${dataset}_1_${stream}.t7 --pth ../models/${dataset}_1_${stream}.pth
+        python convert_torch.py --t7 ${dataset}_1_${stream}.t7 --pth ${dataset}_1_${stream}.pth
     else
         exit 1
     fi
